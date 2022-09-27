f1239e8a49c9d4b71d1dc3190159b03cceffd432
C0B21063 <c0b2106331@edu.teu.ac.jp>
2022-09-27 17:16:47 +0900

    コメント

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,23 +1,26 @@
 import random
 import time
 
-num = 10
-nnum = 2
-num_try = 2
+num = 10 #対象文字数
+nnum = 2 #欠損文字数
+num_try = 2 #チャレンジ回数
 
 def syutudai(alpha):
-    taisyo = random.sample(alpha,num)
+    # 全アルファベットから、対象文字をランダムに10文字選ぶ
+    taisyo = random.sample(alpha, num)
     print("対象文字:")
     for c in taisyo:
         print(c, end="")
-    print("")
+    print()
+
+    # 対象10文字をシャッフルし、欠損文字をランダムに2文字選ぶ
     print("表示文字:")
     random.shuffle(taisyo)
     kesson = random.sample(taisyo,nnum)
     for c in taisyo:
         if c not in kesson:
             print(c,end="")
-    print("")
+    print()
     return kesson
 
 def kotae(kesson):
