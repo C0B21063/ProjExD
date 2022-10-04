829239f5ea8dd1384cb8a2ec801a27978f22acc5
C0B21063 <c0b2106331@edu.teu.ac.jp>
2022-10-04 14:51:58 +0900

    4

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,7 +7,10 @@ def button_click(event):
     tkm.showinfo("",f"{num}のボタンがクリックされました。")
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("300x600")
+
+entry = tk.Entry(root, justify = "right", width = 10, font = ("Times New Roman", 40))
+entry.grid(row = 0, column = 0, columnspan = 3)
 
 r,c = 0,0
 for i in range(9,-1,-1):
@@ -18,7 +21,7 @@ for i in range(9,-1,-1):
                     height = 2
                     )
     button.bind("<1>", button_click)
-    button.grid(row = r, column = c)
+    button.grid(row = r + 1, column = c)
     c += 1
     if i % 3 == 1:
         r += 1
