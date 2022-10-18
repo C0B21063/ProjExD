5bd56f6c4ff858167d43a253548e29af6ecee6f6
C0B21063 <c0b2106331@edu.teu.ac.jp>
2022-10-18 15:19:53 +0900

    11

--- ex03/maze.py
+++ ex03/maze.py
@@ -12,15 +12,17 @@ def key_up(event):
 
 
 def main_proc():
-    global cx, cy
+    global cx, cy, mx, my
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    cx = mx * 100 + 50
+    cy = my * 100 + 50
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
@@ -32,8 +34,13 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width = 1500, height = 900, bg = "black")
     canv.pack()
 
+    maze_lst = mm.make_maze(15, 9)
+    mm.show_maze(canv, maze_lst)
+
     tori = tk.PhotoImage(file = "fig/5.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx = mx * 100 + 50
+    cy = my * 100 + 50
     canv.create_image(cx, cy, image = tori, tag = "tori")
 
     key = "" #現在押されているキーを表す
@@ -42,8 +49,5 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up)
 
     main_proc()
-
-    maze_lst = mm.make_maze(15, 9)
-    mm.show_maze(canv, maze_lst)
     
     root.mainloop()
