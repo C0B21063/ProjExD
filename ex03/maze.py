6c04b5407f9782aea25ef2d6a31a035d85da10e5
C0B21063 <c0b2106331@edu.teu.ac.jp>
2022-10-18 14:29:17 +0900

    6

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,12 @@ def key_down(event):
     global key
     key = event.keysym
 
+
+def key_up(event):
+    global key
+    key = ""
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -18,5 +24,6 @@ if __name__ == "__main__":
     key = "" #現在押されているキーを表す
 
     root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
     
     root.mainloop()
