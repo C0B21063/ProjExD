34c9e44ac27e1ce2c9d0f6afd52ce97ce05ccca1
C0B21063 <c0b2106331@edu.teu.ac.jp>
2022-10-25 18:00:51 +0900

    #10

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -12,6 +12,7 @@ def check_bound(obj_rct, scr_rct):#obj_ret:鳥rct, 弾rct
     return yoko, tate
 
 
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
